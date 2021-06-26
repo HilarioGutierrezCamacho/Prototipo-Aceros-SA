11b39732fcd1453ca07f05fd06d9e7cfcc39529e
Everardo1 <jose.cenobio@puntosingular.com>
2021-06-26 10:37:47 -0500

    Usando alert (SweetAlert2) ejemplo

---
+++ PrototipoAcerosSA/wwwroot/JS/Alert.js
@@ -0,0 +1,36 @@
+﻿function confirmarEliminar(title, text, icon) {
+    return new Promise(resolve => {
+        Swal.fire({
+            title,
+            text,
+            icon,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonText:'Cancelar',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '¡Si, eliminar!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resolve(result.isConfirmed);
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Eliminación exitosa!',
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            }
+        })
+    }) 
+}
+function confirmarGuardar(title,icon) {
+    return new Promise(resolve => {
+        Swal.fire({
+            title,
+            icon,
+            showConfirmButton: false,
+            timer: 2000
+        }).then((result) => {
+             resolve(result.isConfirmed);
+        })
+    })
+}
