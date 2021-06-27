631d8d09871e31550449ff02933d1103a9ac7194
Everardo1 <jose.cenobio@puntosingular.com>
2021-06-27 09:59:27 -0500

    [Configuraciones] Homologar vistas y mensajes

--- PrototipoAcerosSA/Models/Almacenn.cs
+++ PrototipoAcerosSA/Models/Almacenn.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PrototipoAcerosSA.Models
 {
     public class Almacenn
     {
         public int IdAlmacen { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
         public string Clave { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
         public string Nombre { get; set; }
         public int Ancho { get; set; }
         public int Largo { get; set; }
         public int Alto { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
         public string Estatus { get; set; }
         public string  Descripcion { get; set; }
 
