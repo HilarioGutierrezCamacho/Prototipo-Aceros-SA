a8a10f16aedd804efef1ba2a2fe4cb49bde467e1
HilarioGutierrezCamacho <hilario.gutierrez@puntosingular.com>
2021-06-29 21:57:16 -0500

    [General] Ajuste en menús y add librería validaciones custom

---
+++ PrototipoAcerosSA/Models/Validadores/ValidadorMaximosMinimos.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoAcerosSA.Models.Validadores
+{
+    public class ValidadorMaximosMinimos : AbstractValidator<Articulo>
+    {
+        public ValidadorMaximosMinimos()
+        {
+            RuleFor(x => x.Minimo).LessThan(x => x.Maximo).WithMessage("El minimo debe ser menor al maximo.");
+            RuleFor(x => x.Maximo).GreaterThan(x => x.Minimo).WithMessage("El maximo debe ser mayor al minimo.");
+            RuleFor(x => x.PuntoReorden).GreaterThan(x => x.Minimo).LessThan(x => x.Maximo);
+        }
+    }
+}
